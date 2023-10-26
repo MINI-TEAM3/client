a661b0c5a5b052ec50f361d070b13fe572b6dd58
junehee <dev.junehee@gmail.com>
2023-10-26 14:09:55 +0900

    :sparkles: Feat: 사이드바 텍스트 상수화

---
+++ src/constants/sideBar.ts
@@ -0,0 +1,12 @@
+export const SIDE_BAR_TEXTS = {
+  calendar: '전체 캘린더',
+  request: '요청 내역 확인',
+  myPage: '마이페이지',
+  userInfo: '개인정보 수정',
+  password: '비밀번호 변경',
+  attendance: '나의 근무 관리',
+  annual: '남은 연차',
+  duty: '이번달 당직',
+  logout: '로그아웃',
+  mark: '©Dr.Cal',
+};
