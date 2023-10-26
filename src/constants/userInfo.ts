8f2b10b2c10908c3eac7f6dc9dc34a902ea43f1a
junehee <dev.junehee@gmail.com>
2023-10-26 19:15:01 +0900

    🔨Refactor: 개인정보 수정 컴포넌트 텍스트 상수화

---
+++ src/constants/userInfo.ts
@@ -0,0 +1,21 @@
+export const USER_INFO_TEXTS = Object.freeze({
+  title: {
+    password: '비밀번호 확인',
+    editInfo: '개인정보 수정',
+  },
+  description: '개인정보를 안전하게 보호하기 위해 비밀번호를 다시 한 번 확인해 주세요.',
+  placeholder: '현재 비밀번호를 입력해 주세요.',
+  profileImg: '프로필 이미지',
+  editProfileImg: '변경',
+  name: 'Name',
+  hospital: 'Hospital',
+  dept: 'Department',
+  phone: 'Phone Number',
+  confirm: '확인하기',
+  edit: '수정하기',
+  errors: {
+    profileImg: 'Invalid file type or not a File/Blob',
+    profileImgFileSize: '이미지 용량이 너무 큽니다.\n1MB보다 작은 용량의 이미지를 사용하세요.',
+    failCheckPW: '비밀번호 확인 실패',
+  },
+});
