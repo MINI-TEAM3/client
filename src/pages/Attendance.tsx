98ba62ef5f679349597e4e314317fdfeda5bdc7c
junehee <dev.junehee@gmail.com>
2023-11-07 20:47:04 +0900

    ✨ Feat: 대시보드 컴포넌트 근무 관리 데이터 연결

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,14 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { getAttendance } from '@/lib/api';
+import { AlertState } from '@/lib/types';
+import { alertState } from '@/states/stateAlert';
 import { ATTENDANCE_TEXTS } from '@/constants/attendance';
+import Loading from '@/components/Loading';
+import Alert from '@/components/Alert';
 import DashBoard from '@/components/DashBoard';
 import styled from 'styled-components';
 
 const Attendance = () => {
+  const [dashBoardData, setDashBoardData] = useState({
+    dayWork: '0:00:00',
+    weekWork: '0:00:00',
+    monthWork: '0:00:00',
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const setAlert = useSetRecoilState<AlertState>(alertState);
+
+  // 근무 관리 데이터 호출
+  const getAttendanceData = async () => {
+    try {
+      setIsLoading(true);
+      const res = await getAttendance();
+      if (res.success) {
+        setDashBoardData(res.item);
+      }
+    } catch (error) {
+      setAlert({
+        isOpen: true,
+        content: `근무 관리 데이터 호출 실패\n${error}`,
+        type: 'error',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAttendanceData();
+  }, []);
+
   return (
     <Container>
+      <Alert />
+      {isLoading && <Loading />}
       <Title>
         <h2>{ATTENDANCE_TEXTS.title}</h2>
       </Title>
-      <DashBoard day="9시간 20분" week="85시간 30분" month="123시간 50분" />
+      <DashBoard data={dashBoardData} />
     </Container>
   );
 };
